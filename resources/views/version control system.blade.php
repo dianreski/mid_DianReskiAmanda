92eb860f0b85dbbcf171dab2164a2b3197761193
LAPTOP-N9LI5MDU\HP <dianreskiamanda@gmail.com>
2021-11-09 12:12:24 +0800

    DIANNN

---
+++ resources/views/version control system.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Version control adalah sebuah sistem yang mencatat setiap perubahan terhadap sebuah berkas atau kumpulan berkas sehingga pada suatu saat anda dapat kembali kepada salah satu versi dari berkas tersebut.
+macam-macam version control
+1. CVS
+2. Mercurial
+3. Subversion (SVN)
+4. Bazzar
+5. RCS
+</body>
+</html>
